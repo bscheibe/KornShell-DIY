33122603db28f09571167afccb740117c958d2ba
bscheibe <bscheibe@udel.edu>
2018-09-26 13:31:47 -0400

    Update get_path.h

--- labby2/get_path.h
+++ labby2/get_path.h
@@ -1,44 +1,19 @@
-/*
-  get_path.c
+/* 
+  get_path.h
   Ben Miller
 
-  Just a little sample function that gets the PATH env var, parses it and
-  puts "components" into a linked list, which is returned.
 */
-#include "get_path.h"
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
 
-struct pathelement *get_path()
+/* function prototype.  It returns a pointer to a linked list for the path
+   elements. */
+struct pathelement *get_path();
+
+struct pathelement
 {
-  /* path is a copy of the PATH and p is a temp pointer */
-  char *path, *p;
-
-  /* tmp is a temp point used to create a linked list and pathlist is a
-     pointer to the head of the list */
-  struct pathelement *tmp, *pathlist = NULL;
-
-  p = getenv("PATH");	/* get a pointer to the PATH env var.
-			   make a copy of it, since strtok modifies the
-			   string that it is working with... */
-  path = malloc((strlen(p)+1)*sizeof(char));	/* use malloc(3) */
-  strncpy(path, p, strlen(p));
-  path[strlen(p)] = '\0';
-
-  p = strtok(path, ":"); 	/* PATH is : delimited */
-  do				/* loop through the PATH */
-  {				/* to build a linked list of dirs */
-    if ( !pathlist )		/* create head of list */
-    {
-      tmp = calloc(1, sizeof(struct pathelement));
-      pathlist = tmp;
-    }
-    else			/* add on next element */
-    {
-      tmp->next = calloc(1, sizeof(struct pathelement));
-      tmp = tmp->next;
-    }
-    tmp->element = p;	
-    tmp->next = NULL;
-  } while ( p = strtok(NULL, ":") );
-
-  return pathlist;
-} /* end get_path() */
+  char *element;			/* a dir in the path */
+  struct pathelement *next;		/* pointer to next node */
+};
