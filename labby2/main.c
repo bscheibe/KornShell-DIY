7bcd4ce4cde9b43e33bbfee58670c0e5186c3ff2
Bruce Scheibe <bscheibe@ubuntu-1604.eecis.udel.edu>
2018-09-27 20:32:39 -0400

    Updated code.

--- labby2/main.c
+++ labby2/main.c
@@ -4,14 +4,24 @@
 
 void sig_handler(int signal); 
 
-int main( int argc, char **argv, char **envp )
-{
+int main(int argc, char **argv, char **envp) {
   /* put signal set up stuff here */
+  signal(SIGINT, sig_handler);
+  signal(SIGCLD, sig_handler);
+  signal(SIGTSTP, sig_handler);
 
   return sh(argc, argv, envp);
 }
 
-void sig_handler(int signal)
-{
+void sig_handler(int signal) {
   /* define your signal handler */
+  if (signal == SIGINT) {
+//	printf("\n");
+  }
+  else if (signal == SIGTSTP) {
+//	printf("\n");
+  }
+  else if (signal == SIGCLD) {
+	printf("\n Caught signal %d.", signal);
+  }
 }
---
+++ labby2/which.c
@@ -0,0 +1,5 @@
+#include "which.h"
+
+which () {
+
+}
